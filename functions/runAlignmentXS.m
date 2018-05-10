1027bc8c0ddf380fa96680478b14edea39b7eb96
christian-7 <sieben.christian@googlemail.com>
2018-05-10 22:53:11 +0200

    update align

--- functions/runAlignmentXS.m
+++ functions/runAlignmentXS.m
@@ -1,4 +1,4 @@
-function [aligniter,iterIm,s,n_frame,allIm,data,images0,ParticlesAligned] = runAlignment(MList,data0,imsize,zoomfactor, pixelsize, photonpercount,usfac,angrange,angstep,n_iteration);
+function [aligniter,iterIm,s,n_frame,allIm,data,images0,ParticlesAligned] = runAlignmentXS(MList,data0,imsize,zoomfactor, pixelsize, photonpercount,usfac,angrange,angstep,n_iteration);
 
 % The 2D alignment and averaging code was developed by Xiaoyu Shi@Bo Huang Lab at UCSF
 % The procedure is described in: Shi, Xiaoyu et al., Nature Cell Biology 19.10 (2017): 1178.
